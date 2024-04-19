0bf13684ec34bd8f48568dfa7df0c3894bce1dcc
Aditya Choudhary <adityachoudhary252002@gmail.com>
2024-04-19 14:28:35 +0530

    create different components like home,about,dashboard etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/about'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
