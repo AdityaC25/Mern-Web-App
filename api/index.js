3e7f914ba13608c37e2a8be0057d4f3f4b6b9c0e
Aditya Choudhary <adityachoudhary252002@gmail.com>
2024-06-10 22:04:54 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB_URI).then(
+    () =>{
+        console.log('mongodb is connected!');
+    }
+).catch((err) =>{
+    console.log(err);
+})
 
 
 const PORT = process.env.PORT || 3000
