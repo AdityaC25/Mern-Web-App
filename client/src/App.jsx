35bd5a6a0bc793441b944e4d5b0f36101c8407a3
Aditya Choudhary <adityachoudhary252002@gmail.com>
2024-06-10 21:04:42 +0530

    completed header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import About from './pages/about'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+
+    <Header/>
 
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -18,6 +21,8 @@ const App = () => {
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
+      
+      
 
     </Routes>
     </BrowserRouter>
